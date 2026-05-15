aa998789cb002d47bde357475e9079de84651a6b
amitanand765 <amitanand.765@gmail.com>
2026-05-16 00:54:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -251,3 +251,8 @@ def get_market():
         except:
             continue
     return result
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
